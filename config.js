da5e5e7a2e68336ac791349c66238a37e5e09317
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-09-20 18:33:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {}
 
-config.apiaitoken = 'b42695de653e43cbb9c0fd67f5c7c2c9'
+config.apiaitoken = '5b90171f5b2a4fe3aa835af990b33a6a'
 
 config.facebook = {
-    verification_token : '123456789123',
-    pageAccessToken : 'EAAU92VlQu5gBANE4l4jQ0tKPkObUyAFttybmmnEOapEVA3aiM7TcAWYZCRn1uT4uPhZCJZB4Kg4R8NLoOIIgpdFpWyyBxUVzKXPzZAa2IeuIzCTzRQ6DLZCir7TOElXNZAecJKncWJTp6UVSfEB4ZB8eb4rP3pxzsWIdigX0FbvNAZDZD'
+    verification_token : '1234567890',
+    pageAccessToken : 'EAAUMoaczcF4BAJf2rDOSyXMBEzsOI2OWLeCZCkyZCz5zLIGAApiIzgXNXiuin0v94hiN5SAMV7RZA8mQOguZAvmkT9CdLjC8tOhCyydeS9tBuPIdTn4KcSecmCJFZALmDeWzbZBWnNJR5ZCEU2NLXE6ZBxS7138TdhPVZCxFnWOJhZBwZDZD'
 }
 
 module.exports = config
